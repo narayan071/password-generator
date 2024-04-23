720b46dff74e47d8b2e7f446c6ddb3cd15bcccf4
narayan071 <narayan.pradhan1117@gmail.com>
2024-04-23 14:37:21 +0530

    base project created, added core functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useCallback, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [password, setPassword] = useState("");
+  const [length, setLength] = useState(8);
+  const [charAllow, setCharAllow] = useState(false);
+  const [numAllow, setNumAllow] = useState(false);
 
+  const genPass = useCallback(() => {
+    let base = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if(numAllow) base += "0123456789"
+    if(charAllow) base += "!#$%&()*+,-./:;<=>?@[\\]^_{|}~"
+    let pass = "";
+    for(let i = 0; i<length; i++){
+      let idx = Math.floor(Math.random()*base.length);
+      pass+=base.charAt(idx);
+    }
+    setPassword(pass);
+  }, [length, charAllow, numAllow, setPassword])
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='text-center bg-zinc-600 rounded-md mx-auto my-5 w-full max-w-md'>
+        <h1 className='text-white'>PASSWORD GENERATOR</h1>
+        <input type="text" className='rounded-md' />
+        <button>copy</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
