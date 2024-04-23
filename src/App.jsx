834bd384bc4e97434a80ef9e589d0afe88699461
narayan071 <narayan.pradhan1117@gmail.com>
2024-04-23 15:17:50 +0530

    completed password copy functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState, useRef } from 'react'
 
 function App() {
+  const passref = useRef();
   const [password, setPassword] = useState("");
   const [click, setClick] = useState("click");
   const [length, setLength] = useState(8);
@@ -27,6 +28,11 @@ function App() {
     console.log(pass);
   }, [length, charAllow, numAllow, setPassword])
 
+  const copyPassword = useCallback(()=>{
+    passref.current?.select();
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
   useEffect(()=>{genPass()}, [length, charAllow, numAllow, genPass])
 
   return (
@@ -34,8 +40,16 @@ function App() {
       <div className='text-center text-purple-400 bg-zinc-600 rounded-md mx-auto my-5 w-full max-w-2xl'>
         <div>
           <h1 className='text-white'>PASSWORD GENERATOR</h1>
-          <input type="text" placeholder='password' value={password}  className='rounded-md p-3 my-3' />
-          <button className='bg-blue-600 text-white rounded-md p-3 my-2' onClick={()=>changeClickState()} >{click}</button>
+          <input type="text" placeholder='password' value={password} readOnly  className='rounded-md p-3 my-3' ref = {passref}/>
+          <button 
+  className='bg-blue-600 text-white rounded-md p-3 my-2' 
+  onClick={() => {
+    changeClickState();
+    copyPassword();
+  }}
+>
+  {click}
+</button>
         </div>
         <input type="range" min={6} max={100} className='cursor-pointer m-3' onChange={(e) => {setLength(e.target.value)}} /> <label>length : {length}</label>
         <input type="checkbox" defaultChecked = {false} className='m-3' onChange={(prev)=>setNumAllow(prev => !prev)} /> <label>want numbers?</label>
