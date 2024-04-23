bab9cf28874e911f267cd8192e211e32aebaf249
narayan071 <narayan.pradhan1117@gmail.com>
2024-04-23 14:55:55 +0530

    added password generation and ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 
 function App() {
   const [password, setPassword] = useState("");
@@ -16,14 +16,22 @@ function App() {
       pass+=base.charAt(idx);
     }
     setPassword(pass);
+    console.log(pass);
   }, [length, charAllow, numAllow, setPassword])
-  
+
+  useEffect(()=>{genPass()}, [length, charAllow, numAllow, genPass])
+
   return (
     <>
-      <div className='text-center bg-zinc-600 rounded-md mx-auto my-5 w-full max-w-md'>
-        <h1 className='text-white'>PASSWORD GENERATOR</h1>
-        <input type="text" className='rounded-md' />
-        <button>copy</button>
+      <div className='text-center text-purple-400 bg-zinc-600 rounded-md mx-auto my-5 w-full max-w-2xl'>
+        <div>
+          <h1 className='text-white'>PASSWORD GENERATOR</h1>
+          <input type="text" placeholder='password' value={password}  className='rounded-md p-3 my-3' />
+          <button className='bg-blue-600 text-white rounded-md p-3 my-2'>copy</button>
+        </div>
+        <input type="range" min={6} max={100} className='cursor-pointer m-3' onChange={(e) => {setLength(e.target.value)}} /> <label>length : {length}</label>
+        <input type="checkbox" defaultChecked = {false} className='m-3' onChange={(prev)=>setNumAllow(prev => !prev)} /> <label>want numbers?</label>
+        <input type="checkbox" defaultChecked = {false} className='m-3' onChange={(prev)=>setCharAllow( prev => !prev)}/> <label>want special characters?</label>
       </div>
     </>
   )
